795fe677662c6ab4b5577ce5853701784b1a3734
Purusharth Singh Tomar <ptomar@usc.edu>
2017-09-25 15:31:14 -0700

    Commiting

---
+++ Assets/FireRotateAndAttack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRotateAndAttack : MonoBehaviour {
+
+    public GameObject gem;
+    float targetAngle;
+    
+    // Use this for initialization
+    /*
+     * We want to rotate the fire according to it's position relative to the gem.
+     * gemToFire = gem_position_vector - fire_position_vector
+     * Then we calculate the angle of rotation with respect to positive y axis.
+     */
+	void Start () {
+        Vector3 gemToFire = (this.transform.position - gem.transform.position).normalized;
+        targetAngle = 180f * (float)(Math.Atan2(gemToFire.y,gemToFire.x)/Math.PI); //[-270, 90]
+        
+        transform.Rotate(0f, 0f, targetAngle - 90);
+        this.GetComponent<Rigidbody>().AddForce(gemToFire * 400f);//, ForceMode.Impulse);
+        Destroy(gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(!collision.gameObject.CompareTag("Fire")) //Destroy if collided with anything but fire
+            Destroy(gameObject);
+    }
+}
