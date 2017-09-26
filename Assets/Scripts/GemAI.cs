1432f9128b992fb4d7d9ebe8d0c894fe9363a442
Purusharth Singh Tomar <ptomar@usc.edu>
2017-09-26 00:33:11 -0700

    Commit

---
+++ Assets/Scripts/GemAI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemAI : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject[] fires;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    void Start () {
+		for(int i = 0; i < fires.Length; i++)
+        {
+            fires[i].GetComponent<FireRotateAndAttack>().gem = this.gameObject;
+        }
+        //Instantiate all six fires.
+        Instantiate(fires[0], transform.position + transform.up, transform.rotation);
+        Instantiate(fires[1], transform.position - transform.up, transform.rotation);
+        Instantiate(fires[2], transform.position + transform.right + transform.up, transform.rotation);
+        Instantiate(fires[3], transform.position + transform.up - transform.right, transform.rotation);
+        Instantiate(fires[4], transform.position - transform.up + transform.right, transform.rotation);
+        Instantiate(fires[5], transform.position - transform.up - transform.right, transform.rotation);
+        Instantiate(fires[6], transform.position + transform.right, transform.rotation);
+        Instantiate(fires[7], transform.position - transform.right, transform.rotation);
+
+        StartCoroutine(MyCoroutine());
+
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        for(int i = 1; i <=3; i++)
+        {
+            yield return new WaitForSeconds(3f);
+
+            Instantiate(fires[0], transform.position + transform.up, transform.rotation);
+            Instantiate(fires[1], transform.position - transform.up, transform.rotation);
+            Instantiate(fires[2], transform.position + transform.right + transform.up, transform.rotation);
+            Instantiate(fires[3], transform.position + transform.up - transform.right, transform.rotation);
+            Instantiate(fires[4], transform.position - transform.up + transform.right, transform.rotation);
+            Instantiate(fires[5], transform.position - transform.up - transform.right, transform.rotation);
+            Instantiate(fires[6], transform.position + transform.right, transform.rotation);
+            Instantiate(fires[7], transform.position - transform.right, transform.rotation);
+        }
+
+        yield return new WaitForSeconds(3f);
+        float newX = Random.Range(minX, maxX);
+        float newY = Random.Range(minY, maxY);
+        Vector2 newPosition = new Vector3(newX, newY, transform.position.z);
+        Instantiate(gameObject, newPosition, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovementAndAnimControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementAndAnimControl : MonoBehaviour {
+
+    Animator anim;
+    bool jump;
+    bool isMovingRight;
+    bool prevDir;
+    public float speed;
+    float forwardSpeed;
+    public bool isGrounded;
+    public bool run;
+    SpriteRenderer renderer;
+    //CharacterController controller;
+        
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("jump", jump);
+        anim.SetBool("run", run);
+        run = false;
+        jump = false;
+        isMovingRight = true;
+        prevDir = true; //Say was facing right before as well
+        forwardSpeed = 0;
+        isGrounded = true;
+        renderer = GetComponent<SpriteRenderer>();
+        //controller = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) //Move right
+        {
+            run = true;
+            forwardSpeed = speed;
+            prevDir = isMovingRight;
+            isMovingRight = true;
+            anim.SetBool("jump", jump);
+            anim.SetBool("run", run);
+            transform.position = new Vector3(transform.position.x + forwardSpeed * Time.deltaTime,
+                    transform.position.y, transform.position.z);
+            if (prevDir != isMovingRight) {
+                renderer.flipX = !renderer.flipX;
+            }
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            run = true;
+            forwardSpeed = speed;
+            prevDir = isMovingRight;
+            isMovingRight = false;
+            anim.SetBool("jump", jump);
+            anim.SetBool("run", run);
+            transform.position = new Vector3(transform.position.x - forwardSpeed * Time.deltaTime,
+                    transform.position.y, transform.position.z);
+            if (prevDir != isMovingRight)
+            {
+                renderer.flipX = !renderer.flipX;
+            }
+        }
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+              && isGrounded)
+        {
+            /*
+            Figure out if the player is grounded.
+            If he is then give him a vertical velocity*/
+            GetComponent<Rigidbody>().AddForce(new Vector3(0,400,0));
+            jump = true;
+            run = false;
+            isGrounded = false;
+        }
+
+        anim.SetBool("jump", jump);
+        anim.SetBool("run", run);
+
+        run = false;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
