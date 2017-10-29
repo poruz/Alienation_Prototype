395020d13063029653b2c6a166cb3fa8f0614970
Maureen-Wang <maurwang@gmail.com>
2017-10-29 10:37:24 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/poruz/Alienation_Prototype""
    This reverts commit 5bf167f4c7d9b357cdefcd4d9e3339519a2305ba.

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -7,10 +7,29 @@ public class MenuScript : MonoBehaviour {
 
     public GameObject mText;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject startGameButton;
+    public GameObject startGameText;
+
+    public GameObject quitButton;
+    public GameObject quitText;
+
+    public GameObject controlsButton;
+    public GameObject controlsText;
+
+    public GameObject controlsImage;
+
+    public GameObject returnToMainMenuButton;
+    public GameObject returnToMainMenuText;
+
+    // Use this for initialization
+    void Start () {
+        if (this.name == "ReturnToMainMenuButton")
+        {
+            returnToMainMenuText.SetActive(false);
+            returnToMainMenuButton.SetActive(false);
+            controlsImage.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -36,6 +55,40 @@ public class MenuScript : MonoBehaviour {
             this.gameObject.GetComponent<Renderer>().enabled = false;
             mText.gameObject.GetComponent<Renderer>().enabled = false;
             UnityEngine.SceneManagement.SceneManager.LoadScene("RitualLevel");
+        }
+        else if (this.gameObject.name == "QuitButton")
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+            mText.gameObject.GetComponent<Renderer>().enabled = false;
+            Application.Quit();
+        }
+        else if (this.gameObject.name == "ControlsButton")
+        {
+            startGameText.SetActive(false);
+            startGameButton.SetActive(false);
+            //GameObject.Find("QuitText").SetActive(false);
+            quitButton.SetActive(false);
+   
+            returnToMainMenuButton.SetActive(true);
+            returnToMainMenuText.SetActive(true);
+            controlsImage.GetComponent<SpriteRenderer>().enabled = true;
+
+            mText.gameObject.SetActive(false);
+            this.gameObject.SetActive(false);
+
+        }
+        else if (this.gameObject.name == "ReturnToMainMenuButton")
+        {
+            startGameButton.SetActive(true);
+            quitButton.SetActive(true);
+            startGameText.SetActive(true);
+            quitText.SetActive(true);
+            controlsButton.SetActive(true);
+            controlsText.SetActive(true);
+
+            mText.gameObject.SetActive(false);
+            controlsImage.GetComponent<SpriteRenderer>().enabled = false;
+            this.gameObject.SetActive(false);
         }
     }
 }
